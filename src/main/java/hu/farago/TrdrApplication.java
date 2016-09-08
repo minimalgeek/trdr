d2f5e1b2ffe17785d9b333264c5c66808d1ea94f
minimalgeek <minimalgeek@gmail.com>
2016-09-08 13:41:11 +0200

    Initial

---
+++ src/main/java/hu/farago/TrdrApplication.java
@@ -0,0 +1,12 @@
+package hu.farago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrdrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrdrApplication.class, args);
+	}
+}
